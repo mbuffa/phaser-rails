71332d80d55436fd3b40b97ebbc75ae867c5211b
Maxime Buffa <mbuffa@gmail.com>
2017-09-16 21:48:02 +0200

    Phaser version 2.8.7 CE.

--- bin/fetch-latest.rb
+++ bin/fetch-latest.rb
@@ -2,19 +2,17 @@
 # coding: UTF-8
 
 # fetch-latest.rb
-# 
+#
 # fetch latest phaser build from github
-# 
+#
 # created on : 2014.04.30.
 # last update: 2016.11.30.
-# 
+#
 # by meinside@gmail.com
 
-PHASER_VERSION = "v2"
-
-JS_URL     = "https://github.com/photonstorm/phaser/raw/master/#{PHASER_VERSION}/build/phaser.js"
-JS_MIN_URL = "https://github.com/photonstorm/phaser/raw/master/#{PHASER_VERSION}/build/phaser.min.js"
-JS_MAP_URL = "https://github.com/photonstorm/phaser/raw/master/#{PHASER_VERSION}/build/phaser.map"
+JS_URL     = "https://raw.githubusercontent.com/photonstorm/phaser-ce/master/build/phaser.js"
+JS_MIN_URL = "https://raw.githubusercontent.com/photonstorm/phaser-ce/master/build/phaser.min.js"
+JS_MAP_URL = "https://raw.githubusercontent.com/photonstorm/phaser-ce/master/build/phaser.map"
 
 JS_DIR     = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor', 'assets', 'javascripts'))
 
@@ -24,4 +22,3 @@ if __FILE__ == $0
     `wget -O '#{filepath}' '#{url}'`
   }
 end
-
